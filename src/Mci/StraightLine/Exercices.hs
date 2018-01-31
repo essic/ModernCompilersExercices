7141f27fcbfaa52add7468c3a24d748c1baee00e
essic <essic@github.com>
2018-01-31 17:22:01 +0100

    chore: exercices from introdution chapter

---
+++ src/Mci/StraightLine/Exercices.hs
@@ -0,0 +1,25 @@
+module Mci.StraightLine.Exercices where
+
+import qualified Mci.Prelude as P
+
+data Tree a = Leaf | Tree (Tree a) a (Tree a)
+
+empty :: P.Ord key => Tree key
+empty = Leaf
+
+insert :: P.Ord key => key -> Tree key-> Tree key
+insert key Leaf = Tree empty key empty
+insert key (Tree left nodeKey right)
+  | key P.< nodeKey = Tree (insert key left) nodeKey right
+  | key P.> nodeKey = Tree left nodeKey (insert key right)
+  | P.otherwise = Tree left key right
+
+member :: P.Ord key => key -> Tree key -> P.Bool
+member _ Leaf =
+  P.False
+
+member key (Tree left nodeKey right)
+  | key P.< nodeKey = member key left
+  | key P.> nodeKey = member key right
+  | P.otherwise = P.True
+
