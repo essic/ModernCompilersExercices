ecaacfdd5960174f0feb78e940773156012169a4
Massyl NAIT-MOULOUD <massil.nait@gmail.com>
2018-01-31 17:00:43 +0100

    Adapting existing test program to work with first step refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,55 @@
 module Main where
 
+import qualified Formatting                   as F
 import           Mci.StraightLine.PrettyPrint
 import           Mci.StraightLine.Syntax
 import           Protolude
+import Mci.StraightLine.Types
+import qualified Data.Map.Strict as Map
+import qualified Control.Monad.State.Strict as State
 
-prog :: Stm
-prog =
- CompoundStm(AssignStm("a",OpExp(NumExp 5, Plus, NumExp 3)),
-  CompoundStm(AssignStm("b",
-      EseqExp(PrintStm[IdExp"a",OpExp(IdExp"a", Minus,NumExp 1)],
-           OpExp(NumExp 10, Times, IdExp"a"))),
-   PrintStm[IdExp "b"]))
-
-prog2 :: Stm
-prog2 =
-  PrintStm [OpExp(NumExp 10,Div, NumExp 2)]
-
-smallTest :: IO ()
-smallTest = do
-  putText "Evaluate max occurence of print statement in :"
-  putText . prettyStm $ prog
-  putText "is ..."
-  putText . show $ maxargs prog
+evaluateWithInfo' :: Stm -> (Stm -> State.StateT Table IO () ) -> IO ()
+evaluateWithInfo' s interpretProgram = do
+  putText "**** Start ****"
+  putText . toSL $ F.format ( "-> " F.% F.text) (prettyStmSL s)
+  State.evalStateT (interpretProgram s) $! Table Map.empty
+  putText $! "Max Args = " <> (show $! maxargs s)
+  putText "**** End ****"
 
 main :: IO ()
-main = interp prog >> interp prog2 >> smallTest
+main = do
+  evaluateWithInfo' prog  interpStm
+  evaluateWithInfo' prog2 interpStm
+  evaluateWithInfo' prog3 interpStm
+  where
+    prog =
+      CompoundStm
+      (AssignStm "a" (OpExp (NumExp 5) Plus (NumExp 3)))
+      (
+        CompoundStm
+        (AssignStm
+         "b"
+         (
+           EseqExp
+           (PrintStm [IdExp"a",OpExp (IdExp "a") Minus (NumExp 1)])
+           (OpExp (NumExp 10) Times (IdExp"a"))
+         )
+        )
+        (PrintStm[IdExp "b"])
+      )
+    prog2 =
+      PrintStm [OpExp (NumExp 10) Div (NumExp 2)]
+    prog3 =
+      CompoundStm
+      (
+        AssignStm
+        "superValue"
+        (
+          EseqExp
+            (PrintStm [IdExp "superValue"])
+            (OpExp (NumExp 10) Times (NumExp 10))
+        )
+      )
+      (
+        PrintStm [IdExp "superValue"]
+      )
