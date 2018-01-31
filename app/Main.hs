182080ac45285f2654daaee767c47f684bf4988f
essic <essic@github.com>
2018-01-31 15:01:08 +0100

    chore: add eveluation logic, with clean output and some example.

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,54 @@
 module Main where
 
+import qualified Formatting                   as F
 import           Mci.StraightLine.Interpretor
 import           Mci.StraightLine.PrettyPrint
 import           Mci.StraightLine.Syntax
-import           Mci.StraightLine.Utils
 import           Protolude
 
-prog :: Stm
-prog =
- CompoundStm
-  (AssignStm "a" (OpExp (NumExp 5) Plus (NumExp 3)))
-  (
-    CompoundStm
-    (AssignStm
-     "b"
-     (
-       EseqExp
-       (PrintStm [IdExp"a",OpExp (IdExp "a") Minus (NumExp 1)])
-       (OpExp (NumExp 10) Times (IdExp"a"))
-     )
-    )
-  (PrintStm[IdExp "b"])
-  )
-
-prog2 :: Stm
-prog2 =
-  PrintStm [OpExp (NumExp 10) Div (NumExp 2)]
-
-smallTest :: IO ()
-smallTest = do
-  putText "Evaluate max occurence of print statement in :"
-  putText . prettyStm $ prog
-  putText "is ..."
-  putText . show $ maxargs prog
+evaluateWithInfo :: Stm -> (Stm -> IO () ) -> IO ()
+evaluateWithInfo s interpretProgram = do
+  putText "**** Start ****"
+  putText . toSL $ F.format ( "-> " F.% F.text) (prettyStmSL s)
+  interpretProgram s
+  putText "**** End ****"
+  pure ()
 
 main :: IO ()
-main =
-  interp prog >> interp prog2 >> smallTest
+main = do
+  evaluateWithInfo prog interp
+  evaluateWithInfo prog2 interp
+  evaluateWithInfo prog3 interp
+  pure ()
+  where
+    prog =
+      CompoundStm
+      (AssignStm "a" (OpExp (NumExp 5) Plus (NumExp 3)))
+      (
+        CompoundStm
+        (AssignStm
+         "b"
+         (
+           EseqExp
+           (PrintStm [IdExp"a",OpExp (IdExp "a") Minus (NumExp 1)])
+           (OpExp (NumExp 10) Times (IdExp"a"))
+         )
+        )
+        (PrintStm[IdExp "b"])
+      )
+    prog2 =
+      PrintStm [OpExp (NumExp 10) Div (NumExp 2)]
+    prog3 =
+      CompoundStm
+      (
+        AssignStm
+        "superValue"
+        (
+          EseqExp
+            (PrintStm [IdExp "superValue"])
+            (OpExp (NumExp 10) Times (NumExp 10))
+        )
+      )
+      (
+        PrintStm [IdExp "superValue"]
+      )
