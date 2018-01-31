f76d7d2c4d1b8e8d117b37d5e9cadaff0a682d16
Massyl NAIT-MOULOUD <massil.nait@gmail.com>
2018-01-31 16:35:30 +0100

    Fixing Compilation errors in PrettyPrint

--- src/Mci/StraightLine/PrettyPrint.hs
+++ src/Mci/StraightLine/PrettyPrint.hs
@@ -4,10 +4,11 @@ module Mci.StraightLine.PrettyPrint where
 import           Data.Foldable           (foldl1)
 import qualified Formatting              as F
 import           Mci.Prelude
-import           Mci.StraightLine.Syntax
+
+import Mci.StraightLine.Types
 
 prettyStmSL :: Stm -> LText
-prettyStmSL (AssignStm (id,expr)) =
+prettyStmSL (AssignStm id expr) =
   F.format (F.text F.% " := " F.% F.text) (toS id) (prettyExpSL expr)
 
 prettyStmSL (PrintStm expList) = F.format ( "print (" F.% F.text F.% ")") (printParams expList)
@@ -16,16 +17,16 @@ prettyStmSL (PrintStm expList) = F.format ( "print (" F.% F.text F.% ")") (print
       printParams [e] = prettyExpSL e
       printParams x = foldl1 (F.format ( F.text F.% "," F.% F.text)) $ map prettyExpSL x
 
-prettyStmSL (CompoundStm (s1,s2)) =
+prettyStmSL (CompoundStm s1 s2) =
   F.format (F.text F.% " ; " F.% F.text ) (prettyStmSL s1) (prettyStmSL s2)
 
 
 prettyExpSL :: Exp -> LText
 prettyExpSL (IdExp val) = toSL val
 prettyExpSL (NumExp val) = show val
-prettyExpSL (OpExp (expr1,op,expr2)) =
+prettyExpSL (OpExp expr1 op expr2) =
   F.format ( "" F.% F.text F.% F.text F.% F.text ) (prettyExpSL expr1) (prettyBinOpSL op) (prettyExpSL expr2)
-prettyExpSL (EseqExp (stm,expr)) =
+prettyExpSL (EseqExp stm expr) =
   F.format ( "( " F.% F.text F.% " , " F.% F.text F.% " ) ") (prettyStmSL stm) (prettyExpSL expr)
 
 prettyBinOpSL :: Binop -> LText
