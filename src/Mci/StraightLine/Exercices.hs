94a7e0e69bfd9075376de80de2e2fad78a6d9a70
essic <essic@users.noreply.github.com>
2018-06-28 00:48:59 +0200

    make 'printable' results of functions

--- src/Mci/StraightLine/Exercices.hs
+++ src/Mci/StraightLine/Exercices.hs
@@ -3,18 +3,19 @@ module Mci.StraightLine.Exercices where
 import qualified Mci.Prelude as P
 
 data Tree a = Leaf | Tree (Tree a) a (Tree a)
+  deriving P.Show
 
-empty :: P.Ord key => Tree key
+empty :: (P.Ord key, P.Show key) => Tree key
 empty = Leaf
 
-insert :: P.Ord key => key -> Tree key-> Tree key
+insert :: (P.Ord key, P.Show key) => key -> Tree key-> Tree key
 insert key Leaf = Tree empty key empty
 insert key (Tree left nodeKey right)
   | key P.< nodeKey = Tree (insert key left) nodeKey right
   | key P.> nodeKey = Tree left nodeKey (insert key right)
   | P.otherwise = Tree left key right
 
-member :: P.Ord key => key -> Tree key -> P.Bool
+member :: (P.Ord key,P.Show key) => key -> Tree key -> P.Bool
 member _ Leaf =
   P.False
 
